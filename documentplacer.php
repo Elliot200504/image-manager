bd0029227a4d1f1eec5f348a8c84ea63eb2026f3
Elliot <Elliot.astrand@hotmail.com>
2025-10-24 13:59:17 +0200

    fix: clickable link + placing of buttons

--- documentplacer.php
+++ documentplacer.php
@@ -37,32 +37,40 @@ if (file_exists(__DIR__ . '/documents.json')) {
     <tr class="image-row" draggable="true"
         data-bild-id="<?= htmlspecialchars($img['Bild_ID'] ?? 'Bild_ID_' . ($idx + 1)) ?>"
         data-order="<?= htmlspecialchars($img['order'] ?? ($idx + 1)) ?>">
-        <td style="width:40px;">
+        <td style="width:36px; vertical-align: top;">
             <div class="order-square"><?= $idx + 1 ?></div>
         </td>
-        <td style="width:72px;">
+        <td style="width:120px; vertical-align: top;">
             <img src="uploads/<?= htmlspecialchars($img['filename']) ?>"
                  alt="<?= htmlspecialchars($img['Bild_ID'] ?? 'Bild') ?>"
                  class="table-thumb"
                  data-gallery-index="<?= $idx ?>">
         </td>
-        <td>
-            <span class="filename"><?= htmlspecialchars($img['filename']) ?></span>
-        </td>
-        <td class="actions">
-            <a href="uploads/<?= htmlspecialchars($img['filename']) ?>" download title="Ladda ner">
-                <i class="download icon"></i>
+        <td style="vertical-align: top;">
+            <a href="uploads/<?= htmlspecialchars($img['filename']) ?>"
+               class="filename-link"
+               target="_blank">
+                <?= htmlspecialchars($img['filename']) ?>
             </a>
-            <button class="ui icon button rotate-btn" title="Rotera">
-                <i class="undo icon"></i>
-            </button>
-            <button class="ui icon button edit-btn" title="Redigera">
-                <i class="pencil alternate icon"></i>
-            </button>
-            <button class="ui icon button delete-btn" title="Ta bort">
-                <i class="trash icon"></i>
-            </button>
         </td>
+        <td class="actions-cell" style="vertical-align: top; text-align: right; position: relative;">
+            <div class="actions">
+                <a href="uploads/<?= htmlspecialchars($img['filename']) ?>" download title="Ladda ner">
+                    <i class="download icon"></i>
+                </a>
+                <button class="ui icon button rotate-btn" title="Rotera">
+                    <i class="undo icon"></i>
+                </button>
+                <button class="ui icon button edit-btn" title="Redigera">
+                    <i class="pencil alternate icon"></i>
+                </button>
+                <button class="ui icon button delete-btn" title="Ta bort">
+                    <i class="trash icon"></i>
+                </button>
+            </div>
+        </td>
+    </tr>
+    <tr>
     </tr>
 <?php endforeach; ?>
 </tbody>
