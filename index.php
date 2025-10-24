06ce48b2151d0d43bebce666c10cd1a87c112457
Elliot <Elliot.astrand@hotmail.com>
2025-10-24 11:24:18 +0200

    include image gallery

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <title>Bild Manager</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.8/semantic.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/gallery.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.8/semantic.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
+    <script src="assets/js/gallery.js"></script>
     <script src="assets/js/main.js"></script>
 </head>
 <body>
