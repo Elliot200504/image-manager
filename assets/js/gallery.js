d5179af9d19a4b8226952cd7488dd95519ace1fc
Elliot <Elliot.astrand@hotmail.com>
2026-03-08 21:34:01 +0100

    updated gallery integration with new image handling

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -45,9 +45,10 @@ class ImageGallery {
     });
   }
 
-  open(images, startIndex = 0) {
+  open(images, startIndex = 0, meta = null) {
     this.images = images;
     this.currentIndex = startIndex;
+    this.meta = meta || [];
     this.updateDisplay();
     this.$modal.modal('show');
   }
@@ -76,6 +77,26 @@ class ImageGallery {
     this.$modal.find('#galleryCounter').text(
       `${this.currentIndex + 1} / ${this.images.length}`
     );
+        // Custom overlay
+        let overlay = '';
+        if (this.meta && this.meta[this.currentIndex]) {
+          const m = this.meta[this.currentIndex];
+          overlay = `
+            <div id="galleryOverlay" style="position:absolute;top:0;left:0;width:100%;height:100%;pointer-events:none;">
+              <div class="gallery-info-top">
+                <div class="gallery-title">${m.title || '<span style=\"color:#eee;\">No title</span>'}</div>
+                <div class="gallery-author">by ${m.username || 'Unknown'}</div>
+              </div>
+              <div class="gallery-info-bottom">
+                <a href="${m.download}" download="${m.filename}" class="ui button gallery-download">Download <i class="download icon"></i></a>
+                <div class="gallery-type">Type: ${m.type || 'Unknown'}</div>
+              </div>
+            </div>
+          `;
+    }
+    this.$modal.find('#galleryOverlay').remove();
+    this.$modal.find('#galleryImageContainer').css('position', 'relative');
+    this.$modal.find('#galleryImageContainer').append(overlay);
   }
 }
 
@@ -88,35 +109,7 @@ function initiateImageGallery() {
   $('#btnGallery').on('click', function (e) {
     e.preventDefault();
 
-    // Collect all images
-    const images = [];
-    $('#imglist img').each(function () {
-      const src = $(this).attr('src');
-      if (src && window.imageGallery && window.imageGallery.isImageFile(src)) {
-        images.push(src);
-      }
-    });
-
-    $('#imglist a[href]').each(function () {
-      const href = $(this).attr('href');
-      if (
-        href &&
-        window.imageGallery &&
-        window.imageGallery.isImageFile(href)
-      ) {
-        if (!images.includes(href)) {
-          images.push(href);
-        }
-      }
-    });
-
-    if (images.length === 0) {
-      showAlert({
-        header: i18next.t('Ingen bild'),
-        body: i18next.t('Inga bilder att visa i galleriet'),
-      });
-      return;
-    }
+    // ...existing code...
 
     if (window.imageGallery) {
       window.imageGallery.open(images, 0);
